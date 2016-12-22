d254b6a49b3724d8d47cc6e54d3576a6fe9dffe0
Oleg Lokshyn <olokshyn@gmail.com>
2016-12-23 01:33:23 +0200

    Added Branch and Bound to main menu.

--- Main.py
+++ Main.py
@@ -2,12 +2,13 @@ import sys
 
 from Graph import Graph
 from Regulation import Regulation
+from BranchAndBound import BranchAndBound
 from GraphRepr import GraphReprReader
 
 
-def print_results(r):
-    target_l = raw_input('Enter desired l: ')
-    h, regulation = r.minimize_h(int(target_l))
+def print_regulations(graph, target_l):
+    r = Regulation(graph)
+    h, regulation = r.minimize_h(target_l)
 
     print 'S-lower:'
     for row in r.s_lower:
@@ -25,15 +26,23 @@ def print_results(r):
         print ' '.join([str(x) for x in row])
 
 
+def print_branch_and_bound(graph, target_h):
+    bb = BranchAndBound(target_h, graph)
+    solutions = bb.solve()
+    print 'Solutions with h = {0}:'.format(target_h)
+    for solution in solutions:
+        for row in solution:
+            print ' '.join([str(x) for x in row])
+        print ''
+
+
 def load_graph():
     filename = raw_input('Enter the path to the graph file: ')
-    print_results(Regulation(Graph(
-        GraphReprReader.read_from_file(filename))))
+    return Graph(GraphReprReader.read_from_file(filename))
 
 
 def enter_graph():
-    print_results(Regulation(Graph(
-        GraphReprReader.read_from_opened_file('al', sys.stdin))))
+    return GraphReprReader.read_from_opened_file('al', sys.stdin)
 
 
 def main():
@@ -42,11 +51,22 @@ def main():
                          'load - load graph from file\n'
                          'read - read graph from input\n')
         if mode == 'load':
-            load_graph()
+            graph = load_graph()
         elif mode == 'read':
-            enter_graph()
+            graph = enter_graph()
         else:
             raise ValueError('Unknown mode')
+
+        mode = raw_input('Chose mode:\n'
+                         'h - minimize h\n'
+                         'l - minimize l\n')
+        if mode == 'h':
+            target_l = int(raw_input('Enter target l: '))
+            print_regulations(graph, target_l)
+        elif mode == 'l':
+            target_h = int(raw_input('Enter target h: '))
+            print_branch_and_bound(graph, target_h)
+
         return 0
     except Exception as exc:
         print exc.message
